c3d03e75b2c8f08b2992cfbd2ca8211ce3e23238
Alexei Goja <agoja@adyax.com>
2014-07-22 11:45:39 +0300

    Changes in blog tpl

--- sites/all/themes/uhappy_theme/templates/node/node--blog.tpl.php
+++ sites/all/themes/uhappy_theme/templates/node/node--blog.tpl.php
@@ -89,26 +89,24 @@
         <?php print render($content['field_image']); ?>
       </div>
       <div class="right">
-        <header>
-          <?php print render($title_prefix); ?>
-          <?php if (!$page): ?>
-            <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
-          <?php endif; ?>
-          <?php print render($title_suffix); ?>
-
-          <?php if ($display_submitted): ?>
-            <div class="submitted">
-              <?php print $submitted; ?>
-            </div>
-          <?php endif; ?>
-        </header>
+        <?php if ($title): ?>
+          <header>
+            <?php print render($title_prefix); ?>
+            <h2><?php print $title; ?></h2>
+            <?php if ($display_submitted): ?>
+              <div class="submitted">
+                <?php print $submitted; ?>
+              </div>
+            <?php endif; ?>
+          </header>
+        <?php endif; ?>
         <?php
         // We hide the comments and links now so that we can render them later.
         hide($content['comments']);
         hide($content['links']);
         print render($content);
         ?>
-        <?php print render($content['links']); ?>
+        <?php hide($content['links']); ?>
       </div>
     </div>
   </div>
